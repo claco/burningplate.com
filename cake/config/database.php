90f71ca114cb10643df3fea4955bcee7c37a24f2
Christopher H. Laco <claco@chrislaco.com>
2008-12-27 20:58:57 -0500

    Added cakephp starter

---
+++ cake/config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'sqlite',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'cake',
+	);
+}
+?>
