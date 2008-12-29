ff3211662a6d758d493cbb2c11d12bcd367c13b0
Christopher H. Laco <claco@chrislaco.com>
2008-12-28 20:23:14 -0500

    Added default layout

---
+++ symfony/apps/bp/modules/Home/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Home actions.
+ *
+ * @package    .
+ * @subpackage Home
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class HomeActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+
+  }
+}
--- symfony/apps/bp/templates/layout.php
+++ symfony/apps/bp/templates/layout.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
-    <link rel="shortcut icon" href="/favicon.ico" />
-  </head>
-  <body>
-    <?php echo $sf_content ?>
-  </body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title><?php echo sfConfig::get('app_name'); ?>: <?php echo $this->name ?></title>
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/reset/reset-min.css">
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.6.0/build/base/base-min.css">
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<h1><?php echo sfConfig::get('app_name'); ?></h1>
+				<h2><?php echo $this->name ?></h2>
+			</div>
+			<div id="content">
+				<?php echo $sf_content ?>
+			</div>
+			<div id="footer">
+
+			</div>
+		</div>
+	</body>
 </html>
